0ada1b55452af1700f71499717634b1e5f941626
MariamMagdy <1501410@eng.asu.edu.eg>
2020-05-11 20:44:06 +0200

    recognize text from image

--- final final update.py
+++ final final update.py
@@ -27,18 +27,15 @@ import re
 
 
 # Recognize text with tesseract for python
-
-
+# path of Tesseract-OCR program on my computer
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 
 
-# Path of working folder on Disk
+# Path  of charging photos on my computer
 
-
-
-src_path = r"C:/Program Files/Tesseract-OCR/Temp/Temp2/" # source of images
+src_path = r"C:/Program Files/Tesseract-OCR/Temp/Temp2/"
 
 
 
@@ -52,115 +49,42 @@ def get_string(img_path):
 
     # Read image with opencv
 
-
-
     img = cv2.imread(img_path)
 
 
 
-
-
-
-
-    # Convert to gray
-
-
+    # Convert image from colors  to gray
 
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
-
-
-
-
-
-    # Apply dilation and erosion to remove some noise
-
-
-
-    #kernel = np.ones((2, 2), np.uint8)
-
-
+    # Apply  erosion on the image to remove some noise using elipse structuring element
 
     kernel =cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
-
-
-
-
-
-
-
-    #img = cv2.dilate(img, kernel, iterations=1)
-
-
 
     img_erode = cv2.erode(img, kernel, iterations=1)
 
 
 
 
-
-
-
-    # Write image after removed noise
-
-
-
-    #plt.imshow(img1)
-
-
+    # see the  image after removing noise
 
     #cv2.imshow('removed_noise.png',img_erode)
 
-
-
     #cv2.waitKey(0)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
     # Apply threshold to get image with only black and white
 
-
-
     (thresh,img_threshold) = cv2.threshold(img, 117, 255, cv2.THRESH_BINARY)
 
 
-
-    #img_threshold = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 33, 2)
-
-
+    # see the  image after applying threshold
 
     #cv2.imshow('image_threshold.png',img_threshold)
 
-
-
     #cv2.waitKey(0)
-
-
-
-
-
-
-
-    # Write the image after apply opencv to do some ...
-
-
-
-    #cv2.imwrite(src_path + "thres.png", img)
-
-
 
 
 
@@ -168,20 +92,10 @@ def get_string(img_path):
 
     # Recognize text with tesseract for python
 
-
-
     result = pytesseract.image_to_string(img_path,lang='eng+ara')
 
 
-
-
-
-
-
-
-
-
-
+   # return the text recognized from image 
     return result
 
 
