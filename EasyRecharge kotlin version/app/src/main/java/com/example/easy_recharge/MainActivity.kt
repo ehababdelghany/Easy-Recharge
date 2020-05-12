dda2e3d1e6278dbcc9ee208042bfb44dbbc6e0fe
Ehab Abd Elghany <hobadxx@gmail.com>
2020-05-12 05:42:53 +0200

    fix some bugs in kotlin version

--- EasyRecharge kotlin version/app/src/main/java/com/example/easy_recharge/MainActivity.kt
+++ EasyRecharge kotlin version/app/src/main/java/com/example/easy_recharge/MainActivity.kt
@@ -3,32 +3,36 @@ package com.example.easy_recharge
 
 //import android.support.v7.app.AppCompatActivity
 
-import android.R.attr.bitmap
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.graphics.Rect
 import android.graphics.drawable.BitmapDrawable
+import android.hardware.camera2.CaptureRequest
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.provider.MediaStore
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.google.firebase.ml.vision.text.FirebaseVisionText
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.ByteArrayOutputStream
-import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    var carrier:String?="Etisalat"
+    var carrier:String?="Vodafone"
     var number:String?=""
     var numberAfterCode:String?=""
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -39,7 +43,7 @@ class MainActivity : AppCompatActivity() {
 
         MySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(p0: AdapterView<*>?) {
-
+                add_code()
             }
 
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
@@ -58,10 +62,10 @@ class MainActivity : AppCompatActivity() {
 
         recharge_btn.setOnClickListener {
             //get input from edit text
-            number = editText.text.toString().trim()
+            numberAfterCode = editText.text.toString().trim()
 
             //Dialer intent
-            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + Uri.encode(number)))
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + Uri.encode(numberAfterCode)))
             startActivity(intent)
 
            // val intent = Intent(Intent.ACTION_CALL);
@@ -83,48 +87,51 @@ class MainActivity : AppCompatActivity() {
 
         camera_btn.setOnClickListener {
 
-            var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(i,123)
+           // var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+           // startActivityForResult(i,123)
+            CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(this)
+            //val cropRect: Rect = Rect(0, 0, 1755, 3120)
+            //captureRequestBuilder . set CaptureRequest.SCALER_CROP_REGION, cropRect)
 
 
 
 
         }
-        gallery_btn.setOnClickListener {
-            val j = Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-
-            startActivityForResult(j,124)
-
-
+        imageView.setOnClickListener{
+            CropImage.activity()
+            .setGuidelines(CropImageView.Guidelines.ON)
+            .start(this)
         }
+
+
     }
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode==123)
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE)
         {
-            var img = data?.extras?.get("data") as Bitmap
-            imageView.setImageBitmap(img)
+            var result = CropImage.getActivityResult(data)
+            imageView.setImageURI(result.uri)
             startRecognizing(imageView)
-            add_code()
+            number= editText2.text.toString()
+            Toast.makeText(this@MainActivity, number, LENGTH_LONG).show()
 
-        }
-        else if (requestCode==124)
-        {
-            var img=data?.data
-            imageView.setImageURI(img)
-            startRecognizing(imageView)
             add_code()
         }
+
+
 
 
     }
-    fun add_code()
-    {if (carrier=="Etisalat") {numberAfterCode="*556*"+number+"#"}
-    else if (carrier=="Vodafone"){numberAfterCode="*858*"+number+"#"}
-    else if (carrier=="We"){numberAfterCode="*120*"+number+"#"}
-    else if (carrier=="Orange"){numberAfterCode="#102*"+number+"#"}
+     fun add_code()
+    {   if (carrier=="Etisalat") {numberAfterCode="*556*"+number+"#"}
+        else if (carrier=="Vodafone"){numberAfterCode="*858*"+number+"#"}
+        else if (carrier=="We"){numberAfterCode="*120*"+number+"#"}
+        else if (carrier=="Orange"){numberAfterCode="#102*"+number+"#"}
+        else {numberAfterCode="#103*"+number+"#"}
+        editText.setText("")
         editText.setText(numberAfterCode)
 
 }
@@ -165,6 +172,8 @@ class MainActivity : AppCompatActivity() {
         for (block in resultText.textBlocks) {
             val blockText = block.text
             editText2.append(blockText + "\n")
+            number= editText2.text.toString()
+
         }
     }
 
