a90e47c4b23f8ab3795cf027a2801c224a373b9b
Ehab Abd Elghany <hobadxx@gmail.com>
2020-05-12 18:39:43 +0200

    update readme + last version of kotlin app

--- EasyRecharge kotlin version/app/src/main/java/com/example/easy_recharge/MainActivity.kt
+++ EasyRecharge kotlin version/app/src/main/java/com/example/easy_recharge/MainActivity.kt
@@ -3,16 +3,13 @@ package com.example.easy_recharge
 
 //import android.support.v7.app.AppCompatActivity
 
+import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.Rect
 import android.graphics.drawable.BitmapDrawable
-import android.hardware.camera2.CaptureRequest
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.provider.MediaStore
+import android.telephony.TelephonyManager
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -29,15 +26,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    var carrier:String?="Vodafone"
+    var carrier="Vodafone"
     var number:String?=""
     var numberAfterCode:String?=""
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    //get carrier name
+   // val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+    //var carrier = telephonyManager.networkOperatorName
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //lateinit var imageView: ImageView
-        //lateinit var editText: EditText
+        //spinner is used when carrier is not detected
         val myStrings = arrayOf("Etisalat", "Orange", "We", "Vodafone")
         MySpinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, myStrings)
 
@@ -48,7 +49,9 @@ class MainActivity : AppCompatActivity() {
 
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 Toast.makeText(this@MainActivity, myStrings[p2], LENGTH_LONG).show()
+                //when carrier is not detected user select one operator from spinner box
                 carrier=myStrings[p2]
+                //function to add charging code according to carrier name
                 add_code()
 
             }
@@ -68,14 +71,11 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + Uri.encode(numberAfterCode)))
             startActivity(intent)
 
-           // val intent = Intent(Intent.ACTION_CALL);
-            //intent.data = Uri.parse("tel:$number")
-            //startActivity(intent)
+
 
         }
         share_btn.setOnClickListener {
-            //val cropRect: Rect = Rect(0, 0, 1755, 3120)
-            //captureRequestBuilder . set CaptureRequest.SCALER_CROP_REGION, cropRect)
+            //share button
             val intent= Intent()
             intent.action=Intent.ACTION_SEND
             intent.putExtra(Intent.EXTRA_TEXT,numberAfterCode)
@@ -87,19 +87,18 @@ class MainActivity : AppCompatActivity() {
 
         camera_btn.setOnClickListener {
 
-           // var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-           // startActivityForResult(i,123)
+            //select image from camera or gallery and pass it to crop activity
             CropImage.activity()
                 .setGuidelines(CropImageView.Guidelines.ON)
                 .start(this)
-            //val cropRect: Rect = Rect(0, 0, 1755, 3120)
-            //captureRequestBuilder . set CaptureRequest.SCALER_CROP_REGION, cropRect)
+
 
 
 
 
         }
         imageView.setOnClickListener{
+            //select image from camera or gallery and pass it to crop activity
             CropImage.activity()
             .setGuidelines(CropImageView.Guidelines.ON)
             .start(this)
@@ -112,12 +111,13 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE)
         {
+            //store result image
             var result = CropImage.getActivityResult(data)
+            //show result on imageview
             imageView.setImageURI(result.uri)
+            //start firebase function
             startRecognizing(imageView)
-            number= editText2.text.toString()
-            Toast.makeText(this@MainActivity, number, LENGTH_LONG).show()
-
+            //function to add charging code according to carrier name
             add_code()
         }
 
@@ -126,7 +126,9 @@ class MainActivity : AppCompatActivity() {
 
     }
      fun add_code()
-    {   if (carrier=="Etisalat") {numberAfterCode="*556*"+number+"#"}
+    {   number= editText2.text.toString()
+        Toast.makeText(this@MainActivity, number, LENGTH_LONG).show()
+        if (carrier=="Etisalat") {numberAfterCode="*556*"+number+"#"}
         else if (carrier=="Vodafone"){numberAfterCode="*858*"+number+"#"}
         else if (carrier=="We"){numberAfterCode="*120*"+number+"#"}
         else if (carrier=="Orange"){numberAfterCode="#102*"+number+"#"}
@@ -151,7 +153,7 @@ class MainActivity : AppCompatActivity() {
                 .addOnSuccessListener { firebaseVisionText ->
                     v.isEnabled = true
                     processResultText(firebaseVisionText)
-                    number = firebaseVisionText.text
+                    number= editText2.text.toString()
                 }
                 .addOnFailureListener {
                     v.isEnabled = true
@@ -172,7 +174,7 @@ class MainActivity : AppCompatActivity() {
         for (block in resultText.textBlocks) {
             val blockText = block.text
             editText2.append(blockText + "\n")
-            number= editText2.text.toString()
+
 
         }
     }
--- README.md
+++ README.md
@@ -1,9 +1,62 @@
-# Easy-Recharge
-![](EasyRecharge kotlin version/photos of mobile charging cards/20.jpg)
+
+
+## Easy-Recharge
+![](images/1.jpg)
 
 
 Easy Recharge is an android application that allow users to take photo of scratch card to recharge it automatically 
 there are 2 versions from the application 
-1-react application that upload the image to server with python code that uses openCV and tesseract (finished)
-2-kotlin application that use firebase library and Ml vision(all steps are done locally) (missing some features)
+
+1. react application that upload the image to server with python code that uses openCV and tesseract (finished)
+2. kotlin application that use firebase library and Ml vision(all steps are done locally) (missing some features)
+# commits of all team members appear in branches
+
+# Features 
+
+
+- Capture from camera or upload image from gallery and recharge automatically 
+- Support different types of cards 
+- Recharge codes automatically according to the service provider company
+- Provide optimized , fast mobile android application
+- Share button to share detected serial number (kotlinv ersion)
+
+
+# System Architecture
+![](images/arch.png)
+
+
+# steps 
+1. open the application
+- ![](images/1.jpg)
+
+
+
+2. capture an image or upload from gallery
+- ![](images/etisalat.jpg)
+
+
+3. detect number from card
+- ![](images/3.jpg)
+
+
+4. press recharge button
+- ![](images/et-code.jpg)
+
+
+# test cases
+- test case1
+
+1. ![](images/etisalat.jpg)
+2. ![](images/et-code.jpg)
+
+- test case2
+1. ![](images/orange.jpg)
+2. ![](images/orange-code.jpg)
+
+
+
+
+
+
+
 
